e1c211de39afce245882facb2c3c406dad06cec4
OfficialOnePesewa <153733681+OfficialOnePesewa@users.noreply.github.com>
2026-06-23 20:31:33 +0000

    Create zivpn.sh

---
+++ protocols/zivpn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# OfficialOnePesewa ZIVPN Installer
+clear
+echo "Installing ZIVPN..."
+# Insert original ZIVPN installation steps here
+wget -q https://github.com/xxx/zivpn/raw/main/install.sh -O zivpn-install.sh
+bash zivpn-install.sh
+echo "ZIVPN installed by OfficialOnePesewa"
+read -p "Press Enter to continue..."
