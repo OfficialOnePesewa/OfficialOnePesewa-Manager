a49da3f2dfec9936ecb678d664fd63bb29d41d88
OfficialOnePesewa <153733681+OfficialOnePesewa@users.noreply.github.com>
2026-06-23 20:27:08 +0000

    Add 3D banner script for OfficialOnePesewa

---
+++ lib/banner.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 3D Banner for OfficialOnePesewa
+show_banner() {
+    echo -e "\e[36m"
+    toilet -f smblock --filter border:gay "OfficialOnePesewa"
+    echo -e "\e[0m"
+    echo -e "   \e[1;33mTelegram: @OfficialOnePesewa\e[0m"
+    echo "   =========================================="
+}
